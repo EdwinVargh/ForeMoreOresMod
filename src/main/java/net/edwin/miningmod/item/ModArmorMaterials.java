af47e8cc91ef1b6122ff01ec350f17a6c9a2031a
EdwinVargh <gevarghese.edwin@gmail.com>
2023-08-02 11:24:25 -0500

    Fixed armor

--- src/main/java/net/edwin/miningmod/item/ModArmorMaterials.java
+++ src/main/java/net/edwin/miningmod/item/ModArmorMaterials.java
@@ -1,61 +1,72 @@
 package net.edwin.miningmod.item;
 
+import com.electronwill.nightconfig.core.io.ParsingMode;
 import net.edwin.miningmod.MiningMod;
+import net.minecraft.Util;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.EnumMap;
 import java.util.function.Supplier;
 
 public enum ModArmorMaterials implements ArmorMaterial {
-    JADE("jade", 20, new int[]{2, 5, 7, 3}, 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0.0F, () -> {
+    JADE("jade", 20, Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266655_) -> {
+        p_266655_.put(ArmorItem.Type.BOOTS, 2);
+        p_266655_.put(ArmorItem.Type.LEGGINGS, 5);
+        p_266655_.put(ArmorItem.Type.CHESTPLATE, 7);
+        p_266655_.put(ArmorItem.Type.HELMET, 3);
+    }), 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0.0F, () -> {
         return Ingredient.of(ModItems.JADE.get());
     }),
-    TUNGSTEN("tungsten", 25, new int[]{2, 6, 8, 3}, 10, SoundEvents.ARMOR_EQUIP_NETHERITE, 1.0F, 0.1F, () -> {
+    TUNGSTEN("tungsten", 25, Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266655_) -> {
+        p_266655_.put(ArmorItem.Type.BOOTS, 2);
+        p_266655_.put(ArmorItem.Type.LEGGINGS, 6);
+        p_266655_.put(ArmorItem.Type.CHESTPLATE, 8);
+        p_266655_.put(ArmorItem.Type.HELMET, 3);
+    }), 10, SoundEvents.ARMOR_EQUIP_NETHERITE, 1.0F, 0.1F, () -> {
         return Ingredient.of(ModItems.TUNGSTEN.get());
     });
-    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+
+    public static final StringRepresentable.EnumCodec<ArmorMaterials> CODEC = StringRepresentable.fromEnum(ArmorMaterials::values);
+    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266653_) -> {
+        p_266653_.put(ArmorItem.Type.BOOTS, 13);
+        p_266653_.put(ArmorItem.Type.LEGGINGS, 15);
+        p_266653_.put(ArmorItem.Type.CHESTPLATE, 16);
+        p_266653_.put(ArmorItem.Type.HELMET, 11);
+    });
     private final String name;
     private final int durabilityMultiplier;
-    private final int[] slotProtections;
+    private final EnumMap<ArmorItem.Type, Integer> protectionFunctionForType;
     private final int enchantmentValue;
     private final SoundEvent sound;
     private final float toughness;
     private final float knockbackResistance;
     private final LazyLoadedValue<Ingredient> repairIngredient;
 
-    ModArmorMaterials(String p_40474_, int p_40475_, int[] p_40476_, int p_40477_,
-                      SoundEvent p_40478_, float p_40479_, float p_40480_, Supplier<Ingredient> p_40481_) {
-        this.name = p_40474_;
-        this.durabilityMultiplier = p_40475_;
-        this.slotProtections = p_40476_;
-        this.enchantmentValue = p_40477_;
-        this.sound = p_40478_;
-        this.toughness = p_40479_;
-        this.knockbackResistance = p_40480_;
-        this.repairIngredient = new LazyLoadedValue<>(p_40481_);
+    private ModArmorMaterials(String p_268171_, int p_268303_, EnumMap<ArmorItem.Type, Integer> p_267941_, int p_268086_, SoundEvent p_268145_, float p_268058_, float p_268180_, Supplier<Ingredient> p_268256_) {
+        this.name = p_268171_;
+        this.durabilityMultiplier = p_268303_;
+        this.protectionFunctionForType = p_267941_;
+        this.enchantmentValue = p_268086_;
+        this.sound = p_268145_;
+        this.toughness = p_268058_;
+        this.knockbackResistance = p_268180_;
+        this.repairIngredient = new LazyLoadedValue<>(p_268256_);
     }
 
-    public int getDurabilityForSlot(EquipmentSlot pSlot) {
-        return HEALTH_PER_SLOT[pSlot.getIndex()] * this.durabilityMultiplier;
+    public int getDurabilityForType(ArmorItem.Type p_266745_) {
+        return HEALTH_FUNCTION_FOR_TYPE.get(p_266745_) * this.durabilityMultiplier;
     }
 
-    public int getDefenseForSlot(EquipmentSlot pSlot) {
-        return this.slotProtections[pSlot.getIndex()];
-    }
-
-    @Override
-    public int getDurabilityForType(ArmorItem.Type p_266807_) {
-        return 0;
-    }
-
-    @Override
-    public int getDefenseForType(ArmorItem.Type p_267168_) {
-        return 0;
+    public int getDefenseForType(ArmorItem.Type p_266752_) {
+        return this.protectionFunctionForType.get(p_266752_);
     }
 
     public int getEnchantmentValue() {
@@ -71,7 +82,7 @@ public enum ModArmorMaterials implements ArmorMaterial {
     }
 
     public String getName() {
-        return MiningMod.MOD_ID + ":" + this.name;
+        return this.name;
     }
 
     public float getToughness() {
@@ -80,5 +91,9 @@ public enum ModArmorMaterials implements ArmorMaterial {
 
     public float getKnockbackResistance() {
         return this.knockbackResistance;
+    }
+
+    public String getSerializedName() {
+        return this.name;
     }
 }
