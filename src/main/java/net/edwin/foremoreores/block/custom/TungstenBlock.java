7083c3d72866571da2a504149c257d52e9f596ff
EdwinVargh <gevarghese.edwin@gmail.com>
2023-09-08 13:50:48 -0500

    Fixed Mod ID

--- src/main/java/net/edwin/foremoreores/block/custom/TungstenBlock.java
+++ src/main/java/net/edwin/foremoreores/block/custom/TungstenBlock.java
@@ -1,9 +1,13 @@
 package net.edwin.foremoreores.block.custom;
 
+import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.block.FallingBlock;
 
 public class TungstenBlock extends FallingBlock {
     public TungstenBlock(Properties pProperties) {
         super(pProperties);
+    }
+    protected void falling(FallingBlockEntity p_48779_) {
+        p_48779_.setHurtsEntities(2.0F, 25);
     }
 }
--- src/main/java/net/edwin/foremoreores/datagen/ModBlockLootTables.java
+++ src/main/java/net/edwin/foremoreores/datagen/ModBlockLootTables.java
@@ -3,8 +3,17 @@ package net.edwin.foremoreores.datagen;
 import net.edwin.foremoreores.block.ModBlocks;
 import net.edwin.foremoreores.item.ModItems;
 import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
@@ -16,36 +25,35 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
-        dropSelf(ModBlocks.ALUMINUM_BLOCK.get());
-        dropSelf(ModBlocks.BAUXITE_BLOCK.get());
-        dropSelf(ModBlocks.JADE_BLOCK.get());
-        dropSelf(ModBlocks.LEAD_BLOCK.get());
-        dropSelf(ModBlocks.RAW_LEAD_BLOCK.get());
-        dropSelf(ModBlocks.CUT_LEAD_BLOCK.get());
-        dropSelf(ModBlocks.TUNGSTEN_BLOCK.get());
+        this.dropSelf(ModBlocks.ALUMINUM_BLOCK.get());
+        this.dropSelf(ModBlocks.BAUXITE_BLOCK.get());
+        this.dropSelf(ModBlocks.JADE_BLOCK.get());
+        this.dropSelf(ModBlocks.LEAD_BLOCK.get());
+        this.dropSelf(ModBlocks.RAW_LEAD_BLOCK.get());
+        this.dropSelf(ModBlocks.CUT_LEAD_BLOCK.get());
+        this.dropSelf(ModBlocks.TUNGSTEN_BLOCK.get());
 
-        add(ModBlocks.BAUXITE_ORE.get(),
+        this.add(ModBlocks.BAUXITE_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.BAUXITE_ORE.get(), ModItems.BAUXITE.get()));
-        add(ModBlocks.DEEPSLATE_BAUXITE_ORE.get(),
+        this.add(ModBlocks.DEEPSLATE_BAUXITE_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.DEEPSLATE_BAUXITE_ORE.get(), ModItems.BAUXITE.get()));
-        add(ModBlocks.JADE_ORE.get(),
+        this.add(ModBlocks.JADE_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.JADE_ORE.get(), ModItems.JADE.get()));
-        add(ModBlocks.GALENA.get(),
+        this.add(ModBlocks.GALENA.get(),
                 (block) -> createOreDrop(ModBlocks.GALENA.get(), ModItems.RAW_GALENA.get()));
-        add(ModBlocks.DEEPSLATE_GALENA.get(),
+        this.add(ModBlocks.DEEPSLATE_GALENA.get(),
                 (block) -> createOreDrop(ModBlocks.DEEPSLATE_GALENA.get(), ModItems.RAW_GALENA.get()));
-        add(ModBlocks.REINFORCED_GLASS.get(),
+        this.add(ModBlocks.REINFORCED_GLASS.get(),
                 (block) -> createSilkTouchOnlyTable(ModBlocks.REINFORCED_GLASS.get()));
-        add(ModBlocks.NETHER_GALENA.get(),
+        this.add(ModBlocks.NETHER_GALENA.get(),
                 (block) -> createOreDrop(ModBlocks.DEEPSLATE_GALENA.get(), ModItems.RAW_GALENA.get()));
-        add(ModBlocks.DEEPSLATE_JADE_ORE.get(),
+        this.add(ModBlocks.DEEPSLATE_JADE_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.DEEPSLATE_JADE_ORE.get(), ModItems.JADE.get()));
-        add(ModBlocks.TUNGSTEN_ORE.get(),
+        this.add(ModBlocks.TUNGSTEN_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.TUNGSTEN_ORE.get(), ModItems.TUNGSTEN.get()));
-        add(ModBlocks.DEEPSLATE_TUNGSTEN_ORE.get(),
+        this.add(ModBlocks.DEEPSLATE_TUNGSTEN_ORE.get(),
                 (block) -> createOreDrop(ModBlocks.DEEPSLATE_TUNGSTEN_ORE.get(), ModItems.TUNGSTEN.get()));
     }
-
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
